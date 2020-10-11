2a5ec577b8217955b8ce67147af93639871d3d62
donghee bae <dongheebae@dongheeui-MacBookPro.local>
2020-10-11 12:17:48 +0900

    node eslint prettier 셋팅 추가

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { app } from "./app";
+import { app } from './app';
 
 const start = () => {
   app.listen(3000, () => {
-    console.log("Listening on port 3000!!");
+    console.log('Listening on port 3000!!');
   });
 };
 
