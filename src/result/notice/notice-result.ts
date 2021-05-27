6bf8d6e8cb9aa15de7597348f56a7fe202431a47
brandx-donghee <donghee@brandxcorp.com>
2021-05-27 14:29:10 +0900

    공지사항 네이밍 변경

--- src/result/notice/notice-result.ts
+++ src/result/notice/notice-result.ts
@@ -4,12 +4,12 @@ export class NoticeResult {
   id!: number;
   title!: string;
   content!: string;
-  startAt!: Date;
+  createdAt!: Date;
 
   constructor(data: Notice) {
     this.id = data.id;
     this.title = data.title;
     this.content = data.content;
-    this.startAt = data.startAt;
+    this.createdAt = data.createdAt;
   }
 }
